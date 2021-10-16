30eaebe3c62e01737f2e1359d25e99163ad81b67
Gavin Gregory <gavin.ian.gregory@gmail.com>
2021-10-15 20:10:34 -0400

    message in own file

--- main.ts
+++ main.ts
@@ -1,9 +1,10 @@
 import { Application } from "https://deno.land/x/oak@v9.0.1/mod.ts";
+import { message } from "./content.ts";
 
 const app = new Application();
 
 app.use((ctx) => {
-  ctx.response.body = "Hello everyone!";
+  ctx.response.body = message;
 });
 
 await app.listen({ port: 8000 });
